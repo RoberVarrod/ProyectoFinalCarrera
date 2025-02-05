d7a00de0336469e2606061ced2af2b3db1daa828
RoberVarrod <robervargascr@gmail.com>
2025-02-05 16:09:42 -0600

    Sucursal

--- ProyectoFinal/FrontEnd/Controllers/UsuarioController.cs
+++ ProyectoFinal/FrontEnd/Controllers/UsuarioController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FrontEnd.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FrontEnd.Controllers
 {
     public class UsuarioController : Controller
     {
+
+        private readonly ProyectoPaqueteriaContext _context;
+
+        public UsuarioController(ProyectoPaqueteriaContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Configuracion()
         {
             return View();
@@ -64,10 +74,44 @@ namespace FrontEnd.Controllers
             return View();
         }
 
+        [AllowAnonymous]
+        [HttpGet]
         public IActionResult RegistroSucursal()
         {
             return View();
         }
+
+        [AllowAnonymous]
+        [HttpGet]
+
+        public IActionResult RegistroSucursal(Sucursal modelo) 
+        {
+            if (ModelState.IsValid)
+            {
+              if (_context.Sucursals.Any(u => u.Nombre == modelo.Nombre)) 
+                {
+                    ModelState.AddModelError("", "Ya se selecciono una surcursal.");
+                    return View(modelo);
+                }
+            }
+
+            var nuevaSucursal = new Sucursal
+            {
+                Nombre = modelo.Nombre,
+                Horario = modelo.Horario,
+                Telefono = modelo.Telefono,
+                Direccion = modelo.Direccion
+            };
+
+            _context.Sucursals.Add(nuevaSucursal);
+            _context.SaveChanges();
+            return View();
+        }
+        
+
+
+
+
         public IActionResult ListaSucursal()
         {
             return View();
